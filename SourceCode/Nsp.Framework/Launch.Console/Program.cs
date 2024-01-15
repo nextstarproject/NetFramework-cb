a71ad59e39ae49d67723e86d202f836c9ac8e9e5
AlbertXiaoPeng <albert.xiao@developertools.com>
2024-01-15 18:42:56 +0900

    feat gif add watermark

--- SourceCode/Nsp.Framework/Launch.Console/Program.cs
+++ SourceCode/Nsp.Framework/Launch.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Nsp.Framework.Watermark;
+using Nsp.Framework.Watermark.Magick;
 using Nsp.Framework.Watermark.SkiaSharp;
 using SkiaSharp;
 
@@ -41,4 +42,22 @@ var resource = "Resources";
     SkiaSharpWatermark.Execute(inputPath, outputSinglePath, watermark, WatermarkPosition.Center);
 
     SkiaSharpWatermark.ExecuteFull(inputPath, outputLoopPath, watermark);
+}
+
+{
+    Console.WriteLine("gif watermark");
+    var subDir = "gif-watermark/";
+    Directory.CreateDirectory(subDir);
+    
+    var watermark = Path.Combine(subDir, "watermark.png");
+    SkiaSharpWatermark.GenerateWatermark(watermark, "spiritling.cn");
+    
+    var inputPath = Path.Combine(resource, "chrome_zEjXw3YDnT.gif");
+    var outputPath = Path.Combine(subDir, "chrome_zEjXw3YDnT_output.gif");
+    GifAppendWatermark.Execute(inputPath, outputPath, watermark, (singleImage, watermark) =>
+    {
+        var stream = new MemoryStream();
+        SkiaSharpWatermark.ExecuteFull(singleImage,stream,watermark);
+        return stream;
+    });
 }
---
+++ SourceCode/Nsp.Framework/Nsp.Framework.Watermark/Magick/GifAppendWatermark.cs
@@ -0,0 +1,53 @@
+﻿using ImageMagick;
+
+namespace Nsp.Framework.Watermark.Magick;
+
+public static class GifAppendWatermark
+{
+    public static void Execute(Stream inputStream, string outputPath, Stream watermarkStream,
+        Func<Stream, Stream, Stream> singleWatermarkFunc)
+    {
+        using (var collection = new MagickImageCollection(inputStream))
+        {
+            collection.Coalesce();
+            using (var collection2 = new MagickImageCollection())
+            {
+                foreach (var image in collection)
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        image.Write(stream, MagickFormat.Png);
+                        var newMemoryStream = new MemoryStream(((MemoryStream)watermarkStream).ToArray());
+                        stream.Seek(0, SeekOrigin.Begin); 
+                        var resultStream = singleWatermarkFunc.Invoke(stream, newMemoryStream);
+                        resultStream.Seek(0, SeekOrigin.Begin); // 重置流的位置
+                        var image2 = new MagickImage(resultStream);
+                        collection2.Add(image2);
+                    }
+                }
+
+                // 设置GIF的一些属性，比如循环次数、帧之间的延迟等
+                collection2.OptimizeTransparency(); // 优化透明度
+                collection2.Coalesce(); // 将图像合并为一个动画
+                collection2.Optimize();
+
+                // 保存合并后的GIF文件
+                collection2.Write(outputPath);
+            }
+        }
+    }
+    public static void Execute(string inputPath, string outputPath, string watermarkPath,
+        Func<Stream, Stream, Stream> singleWatermarkFunc)
+    {
+        using var inputFile = File.OpenRead(inputPath);
+        using var inputStream = new MemoryStream();
+        inputFile.CopyTo(inputStream);
+        using var watermarkFile = File.OpenRead(watermarkPath);
+        using var watermarkStream =  new MemoryStream();
+        watermarkFile.CopyTo(watermarkStream);
+        using var outputStream = new MemoryStream();
+        inputStream.Seek(0, SeekOrigin.Begin);
+        watermarkStream.Seek(0, SeekOrigin.Begin);
+        Execute(inputStream, outputPath, watermarkStream, singleWatermarkFunc);
+    }
+}
