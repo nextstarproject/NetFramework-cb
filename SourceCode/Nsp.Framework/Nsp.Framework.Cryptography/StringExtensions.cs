b50514e90e11e1f4a3862b2146f448e57860b521
AlbertXiaoPeng <albert.xiao@developertools.com>
2024-02-21 12:23:27 +0900

    string extension sha encrypt

---
+++ SourceCode/Nsp.Framework/Nsp.Framework.Cryptography/StringExtensions.cs
@@ -0,0 +1,228 @@
+﻿using System.Text;
+
+namespace System;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// 将给定的字符串加密为 md5 十六进制
+    /// </summary>
+    public static string ToMd5([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var md5 = MD5.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = md5.ComputeHash(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var hashByte in hashBytes)
+        {
+            sb.Append(hashByte.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// 将给定的字符串加密为 sh1 十六进制
+    /// </summary>
+    public static string ToSha1([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA1.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var hashByte in hashBytes)
+        {
+            sb.Append(hashByte.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// 将给定的字符串加密为 sha256 十六进制
+    /// </summary>
+    public static string ToSha256([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA256.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var hashByte in hashBytes)
+        {
+            sb.Append(hashByte.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+    
+    /// <summary>
+    /// 将给定的字符串加密为 SHA384 十六进制
+    /// </summary>
+    public static string ToSha384([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA384.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var hashByte in hashBytes)
+        {
+            sb.Append(hashByte.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// 将给定的字符串加密为 sha512 十六进制
+    /// </summary>
+    public static string ToSha512([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA512.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var hashByte in hashBytes)
+        {
+            sb.Append(hashByte.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+    
+    /// <summary>
+    /// 将给定的字符串加密为 SHA3_256 十六进制
+    /// </summary>
+    public static string ToSha3_256([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA3_256.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var hashByte in hashBytes)
+        {
+            sb.Append(hashByte.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+    
+    /// <summary>
+    /// 将给定的字符串加密为 SHA3_384 十六进制
+    /// </summary>
+    public static string ToSha3_384([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA3_384.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var hashByte in hashBytes)
+        {
+            sb.Append(hashByte.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+    
+    /// <summary>
+    /// 将给定的字符串加密为 SHA3_512 十六进制
+    /// </summary>
+    public static string ToSha3_512([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA3_512.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var sb = new StringBuilder();
+        foreach (var hashByte in hashBytes)
+        {
+            sb.Append(hashByte.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// 将给定的字符串加密为 md5 base64
+    /// </summary>
+    public static string ToMd5Base64([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = MD5.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var output = Convert.ToBase64String(hashBytes);
+        return output;
+    }
+
+    /// <summary>
+    /// 将给定的字符串加密为 SHA1 base64
+    /// </summary>
+    public static string ToSha1Base64([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA1.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var output = Convert.ToBase64String(hashBytes);
+        return output;
+    }
+
+    /// <summary>
+    /// 将给定的字符串加密为 SHA256 base64
+    /// </summary>
+    public static string ToSha256Base64([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA256.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var output = Convert.ToBase64String(hashBytes);
+        return output;
+    }
+    
+    /// <summary>
+    /// 将给定的字符串加密为 SHA384 base64
+    /// </summary>
+    public static string ToSha384Base64([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA384.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var output = Convert.ToBase64String(hashBytes);
+        return output;
+    }
+
+    /// <summary>
+    /// 将给定的字符串加密为 SHA512 base64
+    /// </summary>
+    public static string ToSha512Base64([NotNull] this string str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+        using var encrypt = SHA512.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(str);
+        var hashBytes = encrypt.ComputeHash(inputBytes);
+
+        var output = Convert.ToBase64String(hashBytes);
+        return output;
+    }
+}
